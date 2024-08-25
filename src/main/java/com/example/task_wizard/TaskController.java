1671dc562dace2f781b750753054e29195d49cbc
Katleho Lephallo <katleholephallo1@gmail.com>
2024-08-25 14:30:25 +0200

    yaml files for backend, frontend and db

--- src/main/java/com/example/task_wizard/TaskController.java
+++ src/main/java/com/example/task_wizard/TaskController.java
@@ -5,7 +5,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RestController
 @RequestMapping("/tasks")
 public class TaskController {
